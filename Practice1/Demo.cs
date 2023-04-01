bdf1b54919472be867d840b1538e1dd27eb2723d
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-04-01 12:25:07 +0300

    initial commit

---
+++ Practice1/Demo.cs
@@ -0,0 +1,13 @@
+namespace Practice1;
+
+public class Demo
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Main");
+        Solver solver = new Solver();
+        solver.task1();
+    }
+
+
+}
---
+++ Practice1/Solver.cs
@@ -0,0 +1,9 @@
+namespace Practice1;
+
+public class Solver
+{
+    public void task1()
+    {
+        Console.WriteLine("task1");
+    }
+}
